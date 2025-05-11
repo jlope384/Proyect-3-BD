32ba51eba599838eacb59db0d504936d65c2f0c3
jlope384 <lop23415@uvg.edu.gt>
2025-05-11 17:48:04 -0600

    Ahora si todos los endpoints funcionan

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import usuario, asistente, evento, entrada, artista,evento_patrocinador, evento_recurso, evento_artista, patrocinador, registro_asistencia, recurso, rol_usuario, calificacion_evento, lugar, ciudad
+from routes import usuario, asistente, evento, entrada, artista,evento_patrocinador, evento_recurso, evento_artista, patrocinador, registro_asistencia, recurso, rol_usuario, calificacion_evento, lugar, ciudad, tipo_entrada, medio_pago, categoria_evento, tipo_evento, tema_evento
 
 app = FastAPI()
 
@@ -7,7 +7,7 @@ app = FastAPI()
 def ping():
     return {"ping": "pong!"}
 
-# Include the routers from the different modules
+
 app.include_router(artista.router)
 app.include_router(asistente.router)
 app.include_router(calificacion_evento.router) 
@@ -23,8 +23,8 @@ app.include_router(recurso.router)
 app.include_router(rol_usuario.router)
 app.include_router(lugar.router)
 app.include_router(ciudad.router)
-#app.include_router(categoria_evento.router)
-#app.include_router(tipo_evento.router)
-#app.include_router(tema_evento.router)
-#app.include_router(tipo_entrada.router)
-#app.include_router(medio_pago.router)
+app.include_router(categoria_evento.router)
+app.include_router(tipo_evento.router)
+app.include_router(tema_evento.router)
+app.include_router(tipo_entrada.router)
+app.include_router(medio_pago.router)
