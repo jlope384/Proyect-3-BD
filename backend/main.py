08d2bf67dd5ae0e7f2e7ea740ba958cbddd1dda7
jlope384 <lop23415@uvg.edu.gt>
2025-05-11 19:49:44 -0600

    CORS manejados, el anterior fue un html

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-from routes import usuario, asistente, evento, entrada, artista,evento_patrocinador, evento_recurso, evento_artista, patrocinador, registro_asistencia, recurso, rol_usuario, calificacion_evento, lugar, ciudad, tipo_entrada, medio_pago, categoria_evento, tipo_evento, tema_evento
+from fastapi.middleware.cors import CORSMiddleware
+from routes import usuario, asistente, evento, entrada, artista, evento_patrocinador, evento_recurso, evento_artista, patrocinador, registro_asistencia, recurso, rol_usuario, calificacion_evento, lugar, ciudad, tipo_entrada, medio_pago, categoria_evento, tipo_evento, tema_evento
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with specific origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def ping():
     return {"ping": "pong!"}
-
 
 app.include_router(artista.router)
 app.include_router(asistente.router)
