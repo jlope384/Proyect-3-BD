3c0f62015311a1e213e29f7a526ad9a695bd10dd
jlope384 <lop23415@uvg.edu.gt>
2025-05-12 22:43:03 -0600

    Inserts y triggers creados, info de la base de datos cambiada

--- backend/database.py
+++ backend/database.py
@@ -4,9 +4,10 @@ import os
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="eventos_db",
-        user="postgres",
-        password="Javi2004",  
-        port="5432"
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        cursor_factory=RealDictCursor
     )
